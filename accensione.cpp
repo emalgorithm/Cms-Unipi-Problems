335b744ca538ddeec3636a7975958bbd876890c3
emalgorithm <emanuele.rossi1909@gmail.com>
2015-07-17 22:13:46 +0200

    Cleaned some codes

--- accensione.cpp
+++ accensione.cpp
@@ -1,42 +1,4 @@
-#include <vector>
-#include <string>
-#include <string.h>
-#include <functional>
-#include <utility>
-#include <map>
-#include <set>
-#include <queue>
-#include <iostream>
-#include <stdio.h>
-#include <cstdlib>
-#include <math.h>
-#include <algorithm>
-
-using namespace std;
-
-typedef vector< int > vi;
-typedef pair< int, int > ii;
-typedef vector< ii > vii;
-
-#define MAX_N 1000005
-#define INF 9999999
-
-vi divis[MAX_N],curr;
-int prime[MAX_N];
-bool cond = true;
-
-
-void Accendi(int N, int acceso[], int pulsante[]) {
-    for(int i=N;i>=1;i--){
-        for(int j=2;j*i<=N;j++)
-            if(pulsante[j*i])
-                acceso[i] = (acceso[i] + 1)%2;
-        if(acceso[i] == 0)
-            pulsante[i] = (pulsante[i] + 1)%2;
-    }
-}
-
-
-
-
-
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<title>500 Internal Server Error</title>
+<h1>Internal Server Error</h1>
+<p>The server encountered an internal error and was unable to complete your request.  Either the server is overloaded or there is an error in the application.</p>
